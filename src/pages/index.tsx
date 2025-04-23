387ca915d4d867d4f02c95af2d4febdf4a3df555
Ben Milman <bpmilman@gmail.com>
2025-04-23 12:18:42 -0700

    feat: display a card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Card from '@/components/Card';
+
+import { serraAngel } from '@/constants/cards';
+
 export default function Home() {
   return (
     <div>
       <main>
+        <Card
+          card={serraAngel}
+        />
       </main>
     </div>
   );
