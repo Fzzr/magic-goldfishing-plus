9879483c0d2e7c3bf2089bde4e1467416d34a992
Ben Milman <bpmilman@gmail.com>
2025-04-23 12:28:40 -0700

    feat: add CardRow component

---
+++ src/components/CardRow.tsx
@@ -0,0 +1,20 @@
+import 'react';
+
+import type { Card as CardType } from '@/types/card';
+
+import Card from '@/components/Card';
+
+type CardRowProps = {
+  cards: Array<CardType>
+}
+
+const CardRow = ({cards}: CardRowProps) =>
+  <div>
+    {cards.map((card, index) =>
+      <Card
+        key={index}
+        card={card}
+      />)}
+  </div>;
+
+export default CardRow;
