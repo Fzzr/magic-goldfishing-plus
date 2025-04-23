fa227db5e75cf00c66c6bd86bfdd6ec27104757d
Ben Milman <bpmilman@gmail.com>
2025-04-23 12:39:01 -0700

    feat: use PlayArea to display card rows

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,24 @@
-import CardRow from '@/components/CardRow';
+import PlayArea from '@/components/PlayArea';
 
-import { serraAngel } from '@/constants/cards';
+import { GameState } from '@/types/gamestate';
+
+import { island, circleOfProtectionWhite, serraAngel } from '@/constants/cards';
+
+const gameState: GameState = {
+  lands: [island],
+  otherPermanents: [circleOfProtectionWhite],
+  creatures: [serraAngel],
+  turnCounter: 0,
+  opponentLife: 20,
+  playerLife: 20,
+};
 
 export default function Home() {
   return (
     <div>
       <main>
-        <CardRow
-          cards={[serraAngel]}
+        <PlayArea
+          gameState={gameState}
         />
       </main>
     </div>
