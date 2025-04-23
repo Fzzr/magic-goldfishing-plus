9056a3df4cc175d52074f5e9c53fdadf8e190ed2
Ben Milman <bpmilman@gmail.com>
2025-04-23 12:14:39 -0700

    feat: add initial card data constant

---
+++ src/constants/cards.ts
@@ -0,0 +1,13 @@
+import type { Card } from '@/types/card';
+
+const serraAngel: Card = {
+  displayName: 'Serra Angel',
+  manacost: '{3}{W}{W}',
+  typeline: 'Creature — Angel',
+  textbox: 'Flying, vigilance',
+  powerToughness: '4/4',
+};
+
+export {
+  serraAngel,
+};
